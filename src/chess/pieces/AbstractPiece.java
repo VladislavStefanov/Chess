386110e2b88bca0cbb7689ec5d26c596cf56bee8
Vladislav Stefanov <vladislav.stefanov01@sap.com>
2020-04-05 23:13:26 +0300

    Refactor AbstractPiece class
    Signed-off-by: Vladislav Stefanov <vladislav.stefanov01@sap.com>

--- src/chess/pieces/AbstractPiece.java
+++ src/chess/pieces/AbstractPiece.java
@@ -1,40 +1,41 @@
-package pieces;
+package chess.pieces;
 
 public abstract class AbstractPiece {
 
-	boolean isWhite;
+    protected final boolean isWhite;
 
-	/**
-	 * If piece is white set true, false otherwise
-	 */
-	public AbstractPiece(boolean isWhite) {
-		this.isWhite = isWhite;
-	}
+    /**
+     * If piece is white set true, false otherwise
+     */
+    public AbstractPiece(final boolean isWhite) {
+        this.isWhite = isWhite;
+    }
 
-	/**
-	 * Returns true if white, false otherwise
-	 */
-	public boolean isWhite() {
-		// TODO Auto-generated method stub
-		return isWhite;
-	}
+    /**
+     * Returns true if white, false otherwise
+     */
+    public boolean isWhite() {
+        return isWhite;
+    }
 
-	/**
-	 * Draws given piece into the console
-	 */
-	public abstract void draw();
+    /**
+     * Draws given piece into the console
+     */
+    public abstract void draw();
 
-	/**
-	 * Checks whether a given move is valid. Returns true if valid, false
-	 * otherwise
-	 */
-	public abstract boolean isMoveValid(int srcRow, int srcCol, int destRow,
-			int destCol);
+    /**
+     * Checks whether a given move is valid. Returns true if valid, false
+     * otherwise
+     */
+    public abstract boolean isMoveValid(final int sourceRow,
+            final int sourceColumn, final int destinationRow,
+            final int destinationColumn);
 
-	/**
-	 * Returns relative chess piece value of this chessman.
-	 * @return
-	 */
-	public abstract int relativeValue();
+    /**
+     * Returns relative chess piece value of this chessman.
+     *
+     * @return
+     */
+    public abstract int relativeValue();
 
 }
