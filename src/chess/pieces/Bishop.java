0ae7879271f226fc7f8774606f3cb398f4ed0f78
Vladislav Stefanov <vladislav.stefanov01@sap.com>
2020-04-05 23:26:00 +0300

    Remove redundant static method from Bishop class
    Signed-off-by: Vladislav Stefanov <vladislav.stefanov01@sap.com>

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -19,22 +19,15 @@ public class Bishop extends AbstractPiece {
         }
     }
 
-    private static Boolean diagonalPath(final int sourceRow,
-            final int sourceColumn, final int destinationRow,
-            final int destinationColumn) {
+    @Override
+    public boolean isMoveValid(final int sourceRow, final int sourceColumn,
+            final int destinationRow, final int destinationColumn) {
         // returns true if the path is diagonal
         // arguments are initial and final coordinates of move in chessboard
         // array
         // good for checking if a move is valid
         return (Math.abs(sourceRow - destinationRow) == Math
                 .abs(sourceColumn - destinationColumn));
-    }
-
-    @Override
-    public boolean isMoveValid(final int sourceRow, final int sourceColumn,
-            final int destinationRow, final int destinationColumn) {
-        return diagonalPath(sourceRow, sourceColumn, destinationRow,
-                destinationColumn);
     }
 
     @Override
