b4b5f232102ca8b266291f1d977749712a7bd4e1
Vladislav Stefanov <vladislav.stefanov01@sap.com>
2020-04-05 23:25:12 +0300

    Refactor Knight class
    Signed-off-by: Vladislav Stefanov <vladislav.stefanov01@sap.com>

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -1,43 +1,41 @@
 package chess.pieces;
 
-
 public class Knight extends chess.pieces.AbstractPiece {
 
-	public Knight(boolean isWhite) {
-		super(isWhite);
-			}
+    private static final String BLACK_KNIGHT_SYMBLOL = "\u265E";
+    private static final String WHITE_KNIGHT_SYMBLOL = "\u2658";
 
-	@Override
-	public void draw() {
-		if (isWhite){
-			System.out.print("\u2658");
-		}
-		else{
-			System.out.print("\u265E");
-		}		
-	}
-	
-	private static Boolean lShapedPath(int srcRow, int srcCol,
-			int destRow, int destCol) {
-		// returns true if the path is L-shaped
-		// arguments are initial and final coordinates of move in chessboard
-		// array
-		// good for checking if a move is valid
-		return ((Math.abs(srcRow - destRow) == 2 && Math.abs(srcCol
-				- destCol) == 1)
-				|| (Math.abs(srcRow - destRow) == 1 && Math.abs(srcCol
-						- destCol) == 2));
-	}
+    public Knight(final boolean isWhite) {
+        super(isWhite);
+    }
 
-	@Override
-	public boolean isMoveValid(int sourceRow, int sourceColumn, int destinationRow, int destinationColumn) {
-		return lShapedPath(sourceRow, sourceColumn, destinationRow, destinationColumn);
-	}
+    @Override
+    public void draw() {
+        if (isWhite) {
+            System.out.print(WHITE_KNIGHT_SYMBLOL);
+        } else {
+            System.out.print(BLACK_KNIGHT_SYMBLOL);
+        }
+    }
 
-	@Override
-	public int pieceValue() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public boolean isMoveValid(final int sourceRow, final int sourceColumn,
+            final int destinationRow, final int destinationColumn) {
+        // returns true if the path is L-shaped
+        // arguments are initial and final coordinates of move in chessboard
+        // array
+        // good for checking if a move is valid
+
+        return ((Math.abs(sourceRow - destinationRow) == 2
+                && Math.abs(sourceColumn - destinationColumn) == 1)
+                || (Math.abs(sourceRow - destinationRow) == 1
+                        && Math.abs(sourceColumn - destinationColumn) == 2));
+    }
+
+    @Override
+    public int pieceValue() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
 }
